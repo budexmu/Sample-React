860fb0850b609eca2805047399e5d53f934ccee7
Demet Noyan <d_noyan@anadolu.edu.tr>
2018-06-23 16:52:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import Title from './Title';
 import Name from './Name';
@@ -6,37 +5,55 @@ import Surname from './Surname';
 import Submit from './Submit';
 import './App.css';
 import Table from './Table';
+import Edit from './Edit';
 
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { name: '' };
+    this.state = { name: '', surname: '', data: [] };
+  }
 
-}
+  appHhandleChange = (key, value) => {
+    console.log("app handle ", key, value)
+    this.setState({ [key]: value });
+  }
 
-handleChange = () =>  {
-  this.setState({
-    name:this.state.name
-  });
-}
+  appSubmitButton = (e) => { 
+    
+    if (!this.state.name || !this.state.surname) {
+     alert('boş olamaz')
+      return;
+    }
+    console.log('app click');
 
-render() {
-  return (
-    <div className="App">
-      <Title />
-      <Name value={this.state.name} handleSubmit={this.handleChange} />
-      <br />
-      <Surname />
-      <br />
-      <Submit handleChange={this.handleChange} />
-      <p>
-        {JSON.stringify(this.state.fields, null, 2)}
-      </p>
-      <Table />
-    </div>
-  );
-}
+    const item = {
+      name: this.state.name,
+      surname: this.state.surname,
+      edit:this.state.edit,
+    }
+    const data = this.state.data.slice(0);
+    data.push(item);
+    this.setState({ data: data, name: '', surname: '',edit:'' });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span>Name: {this.state.name}</span>
+        <br />
+        <span>Surname: {this.state.surname}</span>
+        <Title />
+        <Name value={this.state.name} handleChange={this.appHhandleChange} />
+        <br />
+        <Surname value={this.state.surname} handleChange={this.appHhandleChange} />
+        <br />
+        <Submit handleChange={this.appSubmitButton} />
+        <Table data={this.state.data} />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
